0bf5fbf1ade8810bd5f1d538fb25e360bcc13f97
xuan vuong <lou1svuong.dev@gmail.com>
2024-08-02 18:13:28 +0700

    refactor : refactor return type

--- src/api/getTransferApt.ts
+++ src/api/getTransferApt.ts
@@ -1,35 +1,36 @@
-import { BASE_URL } from "../utilis/config"
 import { Request, Response } from "express";
 
-export async function getTransferApt(req: Request, res: Response): Promise<void> {
-    try {     
-        const baseHref = `${BASE_URL}/api/actions/transfer-apt`;
-
-        const payload = {
-        title: "Actions Example - Transfer Native Aptos",
-        icon: "https://aptosfoundation.org/brandbook/logotype/PNG/Aptos_Primary_BLK.png",
-        description: "Transfer APT to another APT wallet",
-        links: {
+export async function getTransferApt(
+  req: Request,
+  res: Response
+): Promise<void> {
+  try {
+    const payload = {
+      title: "Actions Example - Transfer Native Aptos",
+      icon: "https://aptosfoundation.org/brandbook/logotype/PNG/Aptos_Primary_BLK.png",
+      description: "Transfer APT to another APT wallet",
+      links: {
         actions: [
-            { label: "Send 1 APT", href: `${baseHref}&amount=1` },
-            { label: "Send 5 APT", href: `${baseHref}&amount=5` },
-            { label: "Send 10 APT", href: `${baseHref}&amount=10` },
-            {
+          { label: "Send 1 APT", amount: 1 },
+          { label: "Send 5 APT", amount: 5 },
+          { label: "Send 10 APT", amount: 10 },
+          {
             label: "Send APT",
-            href: `${baseHref}&amount={amount}`,
             parameters: [
-                {
+              {
                 name: "amount",
                 label: "Enter the amount of APT to send",
                 required: true,
-                },
+              },
             ],
-            },
+          },
         ],
-        },
-        };
-        res.json(payload);
-    } catch (err) {
-        res.status(400).json({ error: (err as Error).message || "An unknown error occurred" });
-    }
+      },
+    };
+    res.json(payload);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ error: (err as Error).message || "An unknown error occurred" });
+  }
 }
