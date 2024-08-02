d405cace8135d3d0259a23a50dab76bf2724ae82
xuan vuong <lou1svuong.dev@gmail.com>
2024-08-02 19:31:01 +0700

    refactor : refactor return type and cors

--- src/api/postTransferApt.ts
+++ src/api/postTransferApt.ts
@@ -1,34 +1,45 @@
-import { AccountAddressInput, AnyNumber, Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+import {
+  AccountAddressInput,
+  AnyNumber,
+  Aptos,
+  AptosConfig,
+  Network,
+} from "@aptos-labs/ts-sdk";
 import { Request, Response } from "express";
 
-export async function postTransferAptos(req: Request, res: Response): Promise<void> {
-    console.log(req.body);
-    try {
-        const config = new AptosConfig({ network: Network.TESTNET });
-        const aptos = new Aptos(config);
-        const { amount, toAddress, fromAddress } = req.body;
+export async function postTransferAptos(
+  req: Request,
+  res: Response
+): Promise<void> {
+  console.log(req.body);
+  try {
+    const config = new AptosConfig({ network: Network.TESTNET });
+    const aptos = new Aptos(config);
+    const { amount, toAddress, fromAddress } = req.body;
 
-        const APTOS_COIN = "0x1::aptos_coin::AptosCoin";
+    const APTOS_COIN = "0x1::aptos_coin::AptosCoin";
 
-        const transaction: any = {
-            data: {
-              function: "0x1::coin::transfer",
-              typeArguments: [APTOS_COIN],
-              functionArguments: [
-                toAddress as AccountAddressInput,
-                Number(amount)*10**8 as AnyNumber,
-              ], // is in Octas
-            },
-        };
+    const transaction: any = {
+      data: {
+        function: "0x1::coin::transfer",
+        typeArguments: [APTOS_COIN],
+        functionArguments: [
+          toAddress as AccountAddressInput,
+          (Number(amount) * 10 ** 8) as AnyNumber,
+        ],
+      },
+    };
 
-        const payload = {
-            transaction: transaction,
-            message: `Send ${amount} APT to ${toAddress}`
-        }
-        
-        console.log("Payload:", payload);
-        res.json(payload);
-    } catch (err) {
-        res.status(400).json({ error: (err as Error).message || "An unknown error occurred" });
-    }
+    const payload = {
+      transaction: transaction,
+      message: `Send ${amount} APT to ${toAddress}`,
+    };
+
+    console.log("Payload:", payload);
+    res.json(payload);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ error: (err as Error).message || "An unknown error occurred" });
+  }
 }
