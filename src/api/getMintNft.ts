945e9c87030ccd302d197611be8c91b5f78edf69
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-25 16:06:11 +0700

    refactor : min get nft and postMint

--- src/api/getMintNft.ts
+++ src/api/getMintNft.ts
@@ -4,13 +4,13 @@ import { Request, Response } from "express";
 
 export async function getMintNft(req: Request, res: Response): Promise<void> {
   try {
-    const baseHref = `${BASE_URL}/api/actions/mint-nft`;
+    const baseHref = `${BASE_URL}/api/actions`;
     const payload = {
       icon: "https://assets-global.website-files.com/606f63778ec431ec1b930f1f/63dbd502218a274f2a602968_aptos.png",
       description: "Mint your own nft",
       title: "Actions Mint NFT",
       links: {
-        actions: [{ label: "Mint", href: `${baseHref}/mint` }],
+        actions: [{ label: "Mint", href: `${baseHref}/mint-nft` }],
       },
     };
     res.json(payload);
--- src/api/postMintNFT.ts
+++ src/api/postMintNFT.ts
@@ -1,45 +1,39 @@
 import {
-    AccountAddressInput,
-    AnyNumber,
-    Aptos,
-    AptosConfig,
-    Network,
-  } from "@aptos-labs/ts-sdk";
-  import { Request, Response } from "express";
-  
-  export async function postMintNFT(
-    req: Request,
-    res: Response
-  ): Promise<void>
-  {
-    console.log("body", req.body);
-    try {
-      const { toAddress, nftName, nftDescription, nftUri } = req.body;
-      const config = new AptosConfig({ network: Network.TESTNET });
-      const aptos = new Aptos(config);
-      const quanity = req.body.quanity;
-      const signer = req.body.signer;
-      const NFT_MODULE = "0x5a44ff76148e3ebdfcedcbf0408ba49844561f7c7db4e5c71e3652d3b7183190::minting::mint_nft";
-      const transaction: any = {
-        data: {
-            function: NFT_MODULE,
-            typeArguments: [],
-            functionArguments: [
-                quanity as AnyNumber,
-            ],
-        }
-      }
-      const payload = {
-        transaction: transaction,
-        message: `Mint Sussces`,
-      };
-  
-      console.log("Payload:", payload);
-      res.json(payload);
-    } catch (err) {
-      res
-        .status(400)
-        .json({ error: (err as Error).message || "An unknown error occurred" });
-    }
+  AccountAddressInput,
+  AnyNumber,
+  Aptos,
+  AptosConfig,
+  Network,
+} from "@aptos-labs/ts-sdk";
+import { Request, Response } from "express";
+
+export async function postMintNFT(req: Request, res: Response): Promise<void> {
+  console.log("body", req.body);
+  try {
+    const { toAddress, nftName, nftDescription, nftUri } = req.body;
+    const config = new AptosConfig({ network: Network.TESTNET });
+    const aptos = new Aptos(config);
+    const quanity: number = 1;
+    const signer = req.body.signer;
+    const NFT_MODULE =
+      "0x5a44ff76148e3ebdfcedcbf0408ba49844561f7c7db4e5c71e3652d3b7183190::minting::mint_nft";
+    const transaction: any = {
+      data: {
+        function: NFT_MODULE,
+        typeArguments: [],
+        functionArguments: [quanity as AnyNumber],
+      },
+    };
+    const payload = {
+      transaction: transaction,
+      message: `Mint Sussces`,
+    };
+
+    console.log("Payload:", payload);
+    res.json(payload);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ error: (err as Error).message || "An unknown error occurred" });
   }
-  
+}
