b156f088af81ddb4dac1f09029fe080e995c4dcc
Kay ( Andrew ) <chauanhtuan185@gmail.com>
2024-09-25 14:06:12 +0700

    feat: add routing

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,17 @@ import { postTransferAptos } from "./api/postTransferApt";
 import { BASE_URL, PORT } from "./utilis/config";
 import { gettAllActionsRegistry } from "./api/gettAllActionsRegistry";
 import { getMintNft } from "./api/getMintNft";
-
+import { getVoting } from "./api/getVoting";
+import { postMintNFT } from './api/postMintNFT'
+import { postVoteTransaction  } from "./api/postVoting";
 const DEFAULT_APT_AMOUNT = 1;
 const ACTIONS_CORS_HEADERS: cors.CorsOptions = {
-  origin: ["http://localhost:3001", "https://x.com"],
+  origin: [
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "https://x.com",
+    "https://action-x-frontend.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "OPTIONS"],
   allowedHeaders: [
     "Content-Type",
@@ -30,7 +37,10 @@ app.get("/api/actions/transfer-apt", getTransferApt);
 app.post("/api/actions/transfer-apt", postTransferAptos);
 app.get("/api/actions/actions-registry/all", gettAllActionsRegistry);
 app.get("/actions.json", getActionsJson);
-app.get("/api/actions/mint-nft",getMintNft);
+app.get("/api/actions/mint-nft", getMintNft);
+app.get("/api/actions/voting", getVoting);
+app.post("/api/actions/mint-nft",postMintNFT);
+app.post("api/actions/voting", postVoteTransaction);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
